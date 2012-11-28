984b88e3ff26253d917cee6b31b32a5bf0eb767a
Bart de Goede <bdegoede@gmail.com>
2012-11-28 17:11:32 +0100

    Box in box function

--- onderwijsinspectie/po_tables.py
+++ onderwijsinspectie/po_tables.py
@@ -1,7 +1,13 @@
 import glob
+from lxml import etree
 import pdfquery
 
 REPORTS = 'rapporten/po'
+
+
+def box_in_box(box1, box2):
+    """ Return True if box 2 fits in box 1 """
+    return float(box1.get('x0')) <= float(box2.get('x0')) and float(box1.get('x1')) >= float(box2.get('x1')) and float(box1.get('y0')) <= float(box2.get('y0')) and float(box1.get('y1')) >= float(box2.get('y1'))
 
 
 def extract_xml(pdf_path):
@@ -20,5 +26,28 @@ def convert_pdfs():
         extract_xml(report)
         print 'Processed %d/%d reports' % (counter + 1, len(reports))
 
+
+def parse_xml(xmlfile):
+    tree = etree.parse(xmlfile)
+    for page in tree.xpath('//LTPage'):
+        aspects = page.xpath('.//LTTextBoxHorizontal[contains(text(), "waliteitsaspect")]')
+        if aspects:
+            for aspect in aspects:
+                print aspect.attrib['bbox']
+    return tree
+
 if __name__ == '__main__':
-    pass
+    box1 = {
+        'x0': '10.0',
+        'x1': '20.0',
+        'y0': '10.0',
+        'y1': '20.0'
+    }
+    box2 = {
+        'x0': '15.0',
+        'x1': '18.0',
+        'y0': '15.0',
+        'y1': '18.0'
+    }
+    print box_in_box(box1, box2)
+    #tree = parse_xml('rapporten/po/xml/owinsp_rapport_amsterdam.xml')
