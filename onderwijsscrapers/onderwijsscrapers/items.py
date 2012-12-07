d35b3a61941c9caa7568b4a641de0ce64bfbe04e
Justin van Wees <justin@vwees.net>
2012-12-07 16:39:11 +0100

    Started addeding duo item

--- onderwijsscrapers/onderwijsscrapers/items.py
+++ onderwijsscrapers/onderwijsscrapers/items.py
@@ -4,11 +4,10 @@ from scrapy.item import Item, Field
 class SchoolItem(Item):
     name = Field()  # `naam`
     address = Field()  # `adres`
-    denomination = Field()  # `denominatie`
     zip_code = Field()  # `postcode`
     city = Field()  # `woonplaats`
     website = Field()  # `website` or `homepage`
-    board = Field()  # `Bestuur` or `Bevoegd gezag`
+    denomination = Field()  # `denominatie`
     education_structure = Field()  # `Onderwijsaanbod`
 
 
@@ -25,6 +24,7 @@ class SchoolVOItem(SchoolItem):
     latitude = Field()  # `latitude`
     phone = Field()  # `telefoon`
     email = Field()  # `e_mail`
+    board = Field()  # `Bestuur` or `Bevoegd gezag`
     schoolvo_status_id = Field()  # `venster_status_id`
     schoolkompas_status_id = Field()  # `schoolkompas_status_id`
     logo_img_url = Field()  # `pad_logo`
@@ -46,6 +46,7 @@ class OnderwijsInspectieItem(SchoolItem):
 
 class VOSchool(OnderwijsInspectieItem):
     brin = Field()  # BRIN-nummer
+    board = Field()  # `Bestuur` or `Bevoegd gezag`
     board_id = Field()  # Bevoegd gezagnummer
     branch_id = Field()  # Vestigingsnummer
     result_card = Field()  # Opbrengstenkaart URL
@@ -53,3 +54,28 @@ class VOSchool(OnderwijsInspectieItem):
 
 class POSchool(OnderwijsInspectieItem):
     pass
+
+
+class DUOSchoolItem(SchoolItem):
+    province = Field()  # `provincie`
+    board_id = Field()  # Bevoegd gezagnummer
+    brin = Field()  # BRIN-nummer
+    branch_id = Field()  # Vestigingsnummer
+    municipality = Field()  # `Gemeente naam`
+    municipality_code = Field()  # `Gemeente nummer`
+    phone = Field()  # `telefoonnummer`
+    correspondence_address = Field()  # `Straatnaam correspondentieadres`
+    correspondence_zip = Field()  # `POSTCODE CORRESPONDENTIEADRES`
+    correspondence_city = Field()  # `PLAATSNAAM CORRESPONDENTIEADRES`
+    nodal_area = Field()  # `NODAAL GEBIED NAAM`
+    nodal_area_code = Field()  # `NODAAL GEBIED CODE`
+    rpa_area = Field()  # `RPA GEBIED NAAM`
+    rpa_area_code = Field()  # `RPA GEBIED CODE`
+    wgr_area = Field()  # `WGR GEBIED NAAM`
+    wgr_area_code = Field()  # `WGR GEBIED CODE`
+    corop_area = Field()  # `COROP GEBIED NAAM`
+    corop_area_code = Field()  # `COROP GEBIED CODE`
+    education_area = Field()  # `ONDERWIJS GEBIED NAAM`
+    education_area_code = Field()  # `ONDERWIJS GEBIED CODE`
+    rmc_area = Field()  # `RMC GEBIED NAAM`
+    rmc_area_code = Field()  # `RMC GEBIED CODE`
