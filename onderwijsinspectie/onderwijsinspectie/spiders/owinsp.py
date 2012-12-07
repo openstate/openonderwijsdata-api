835e329d2e38be40d1e745a9c6d5f94ff02008cc
Bart de Goede <bdegoede@gmail.com>
2012-12-07 14:23:20 +0100

    Moar updates

--- onderwijsinspectie/onderwijsinspectie/spiders/owinsp.py
+++ onderwijsinspectie/onderwijsinspectie/spiders/owinsp.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 import re
 import urlparse
@@ -7,13 +8,13 @@ from scrapy.spider import BaseSpider
 from scrapy.http import Request
 from scrapy.selector import HtmlXPathSelector
 
-from onderwijsinspectie.items import EducationalInstitution
+from onderwijsinspectie.items import VOSchool, POSchool
 
 SCHOOL_ID = re.compile(r'(sch_id=\d+)')
+PAGES = re.compile(r"^Pagina (\d+) van (\d+)$")
 
 
 class OWINSPSpider(BaseSpider):
-    name = 'toezichtkaart.owinsp.nl'
     search_url = 'http://toezichtkaart.owinsp.nl/schoolwijzer/'\
                  'zoekresultaat?xl=0&p1=%%23&p2=maxpg&p3=-1&p1=%%23'\
                  '&p2=hits&p3=-1&p1=sector&p2=%%3D'\
@@ -29,15 +30,8 @@ class OWINSPSpider(BaseSpider):
 
         return re.sub(SCHOOL_ID, _open, url)
 
-    def generate_search_urls(self, zips=settings['ZIPCODES'],
-                    education_sector=settings['EDUCATION_SECTOR']):
-        with open(zips, 'r') as f:
-            search_urls = [self.search_url % {
-                                'education_sector': education_sector,
-                                'zipcode': line.strip()
-                            } for line in f]
-
-        return search_urls
+    def generate_search_urls(self):
+        raise NotImplementedError('Your spider should implement this method.')
 
     def start_requests(self):
         return [Request(url, self.parse_search_results) for url in\
@@ -57,12 +51,7 @@ class OWINSPSpider(BaseSpider):
             # Find sch_id, and append '.22' to unfold content blocks
             url = self.open_blocks(url)
 
-            # First parse education strucure page if we are crawling 'VO'
-            # ('voortgezet onderwijs') institutions
-            if settings['EDUCATION_SECTOR'] == 'VO':
-                yield Request(url, self.parse_education_structure_page)
-            else:
-                yield Request(url, self.follow_closed_blocks)
+            yield Request(url, self.parse_education_structure_page)
 
         pages = hxs.select('//span[@class="pagnr"]/text()').extract()[0]\
                             .strip()
@@ -79,76 +68,12 @@ class OWINSPSpider(BaseSpider):
             yield Request('http://toezichtkaart.owinsp.nl/schoolwijzer/%s'
                 % next_page[0], self.parse_search_results)
 
-    def parse_education_structure_page(self, response):
-        hxs = HtmlXPathSelector(response)
-        structures = hxs.select('//li[@class="match"]/noscript/a')
-
-        # If we end up at a school page directly, yield request immediately
-        if not structures:
-            yield Request(response.url, self.parse_organisation_detail_page)
-
-        for structure in structures:
-            url = 'http://toezichtkaart.owinsp.nl/schoolwijzer/%s'\
-                % structure.select('@href').extract()[0]
-
-            request = Request(url, self.parse_organisation_detail_page)
-            request.meta['item'] = structure.select('text()').extract()[0]
-
-            yield request
-
-    def follow_closed_blocks(self, response):
-        hxs = HtmlXPathSelector(response)
-        tzk = hxs.select('//div[@class="content_main wide"]/div[@class="tzk"]'
-            '/div/ul/li[@class="closed"]/noscript/a')
-
-        # If no (closed) TZK links are found, pass the response to the
-        # organisation detail parser
-        if tzk:
-            req = Request('http://toezichtkaart.owinsp.nl/schoolwijzer/%s'\
-                % tzk.select('@href').extract()[0],
-                self.parse_organisation_detail_page)
-
-            if 'item' in response.meta:
-                req.meta['item'] = response.meta['item']
-
-            yield req
-        else:
-            yield Request(response.url, self.parse_organisation_detail_page)
-
     def parse_organisation_detail_page(self, response):
-        hxs = HtmlXPathSelector(response)
-
-        organisation = EducationalInstitution()
-
-        h_content = hxs.select('//div[@id="hoofd_content"]')
-        organisation['name'] = h_content.select('h1[@class="stitle"]/text()')\
-                            .extract()[0].strip()
-
-        address = h_content.select('./p[@class="detpag"]/text()').extract()
-        address = ', '.join([x.strip() for x in address])
-        organisation['address'] = address.replace(u'\xa0\xa0', u' ')
-        website = h_content.select('ul/li[@class="actlink"]/a/@href').extract()
-        if website:
-            organisation['website'] = website[0]
-
-        organisation['denomination'] = h_content.select('p/em/text()')\
-                            .extract()[0].strip()
-        organisation['rating_excerpt'] = h_content.select('p[3]/text()')\
-                            .extract()[1].strip()
-
-        # Wait... what? Are we going to use an element's top-padding to
-        # get the div we are interested in? Yes we are :(.
-        tzk_rating = hxs.select('//div[@class="content_main wide" and '
-                         '@style="padding-top:0px"]/div[@class="tzk"]')
-        organisation['rating'] = tzk_rating.select('div/text()').extract()[0]
-        r_date = tzk_rating.select('h3/div/text()')\
-                            .extract()[0].replace('\n', ' ').strip()[-10:]
-        organisation['rating_date'] = '%s-%s-%s' % (r_date[-4:], r_date[3:5],
-            r_date[0:2])
+        raise NotImplementedError('Your spider should implement this method.')
 
         # reports = hxs.select('//div[@class="blocknone"]//div[@class="report"]'
         #     '//a')
-        reports = hxs.select('//span[@class="icoon_download"]/a')
+
         if reports:
             organisation['reports'] = []
 
@@ -236,25 +161,97 @@ class VOSpider(OWINSPSpider):
             # Find sch_id, and append '.22' to unfold content blocks
             url = self.open_blocks(url)
 
-            # First parse education structure page if we are crawling 'VO'
-            # ('voortgezet onderwijs') institutions
             yield Request(url, self.parse_education_structure_page)
 
         pages = hxs.select('//span[@class="pagnr"]/text()').extract()[0]\
                             .strip()
 
-        current_page, total_pages = re.compile(r"^Pagina (\d+) van (\d+)$")\
-                                                .search(pages).groups()
+        page_match = PAGES.search(pages)
 
-        if current_page != total_pages:
-            nav_urls = hxs.select('//span[@class="browse"]/noscript//a')
-            if len(nav_urls) > 1:
-                next_page = nav_urls[1].select('@href').extract()
-            else:
-                next_page = nav_urls[0].select('@href').extract()
+        if page_match:
+            current_page, total_pages = page_match.groups()
 
-            yield Request('http://toezichtkaart.owinsp.nl/schoolwijzer/%s'
-                % next_page[0], self.parse_search_results)
+            if current_page != total_pages:
+                nav_urls = hxs.select('//span[@class="browse"]/noscript//a')
+                if len(nav_urls) > 1:
+                    next_page = nav_urls[1].select('@href').extract()
+                else:
+                    next_page = nav_urls[0].select('@href').extract()
+
+                yield Request('http://toezichtkaart.owinsp.nl/schoolwijzer/%s'
+                    % next_page[0], self.parse_search_results)
+
+    def parse_education_structure_page(self, response):
+        """ This method is specific for the VO-schools, as these can have
+        multiple educational structures (vmbo, havo, vwo, ...) """
+        hxs = HtmlXPathSelector(response)
+        structures = hxs.select('//li[@class="match"]/noscript/a')
+
+        # If we end up at a school page directly, yield request immediately
+        if not structures:
+            yield Request(response.url, self.parse_organisation_detail_page)
+
+        for structure in structures:
+            url = 'http://toezichtkaart.owinsp.nl/schoolwijzer/%s'\
+                % structure.select('@href').extract()[0]
+            url = self.open_blocks(url)
+
+            request = Request(url, self.parse_organisation_detail_page)
+            request.meta['item'] = structure.select('text()').extract()[0]
+
+            yield request
+
+    def parse_organisation_detail_page(self, response):
+        hxs = HtmlXPathSelector(response)
+
+        organisation = VOSchool()
+
+        h_content = hxs.select('//div[@id="hoofd_content"]')
+        organisation['name'] = h_content.select('h1[@class="stitle"]/text()')\
+                            .extract()[0].strip()
+
+        address = h_content.select('./p[@class="detpag"]/text()').extract()
+        address = ', '.join([x.strip() for x in address])
+        organisation['address'] = address.replace(u'\xa0\xa0', u' ')
+        website = h_content.select('ul/li[@class="actlink"]/a/@href').extract()
+        if website:
+            organisation['website'] = website[0]
+
+        organisation['denomination'] = h_content.select('p/em/text()')\
+                            .extract()[0].strip()
+        organisation['rating_excerpt'] = h_content.select('p[3]/text()')\
+                            .extract()[1].strip()
+
+        # Wait... what? Are we going to use an element's top-padding to
+        # get the div we are interested in? Yes we are :(.
+        tzk_rating = hxs.select('//div[@class="content_main wide" and '
+                         '@style="padding-top:0px"]/div[@class="tzk"]')
+
+        rating = tzk_rating.select('div/text()').extract()
+        if rating:
+            # There are schools without ratings, such as new schools
+            organisation['rating'] = rating[0]
+
+            r_date = tzk_rating.select('h3/div/text()')\
+                                .extract()[0].replace('\n', ' ').strip()[-10:]
+
+            try:
+                r_date = datetime.strptime(r_date, '%d-%m-%Y')
+                r_date = r_date.date().isoformat()
+            except:
+                print
+                print
+                print r_date
+
+            organisation['rating_date'] = r_date
+
+        reports = hxs.select('//div[@class="report" and span'
+                             '[@class="icoon_pdf2"]]/span'
+                             '[@class="icoon_download"]/a')
+
+        print
+        print
+        print reports
 
 
 class POSpider(OWINSPSpider):
