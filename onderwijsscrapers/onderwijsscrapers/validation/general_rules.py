fdead997d960f401fe355f633c185ebd9394e40f
Justin van Wees <justin@vwees.net>
2013-02-10 17:41:08 +0100

    Added some general validation rules

--- onderwijsscrapers/onderwijsscrapers/validation/general_rules.py
+++ onderwijsscrapers/onderwijsscrapers/validation/general_rules.py
@@ -1,7 +1,20 @@
-from colander import MappingSchema, SchemaNode, String, Length
+from colander import MappingSchema, SequenceSchema, SchemaNode, String, Length, Int, url
 
+board_id = SchemaNode(Int())
+brin = SchemaNode(String(), validator=Length(min=4, max=4))
+name = SchemaNode(String(), validator=Length(min=4, max=300))
+phone = SchemaNode(String(), validator=Length(min=10, max=15))
+denomination = SchemaNode(String(), validator=Length(min=5, max=150))
+municipality = SchemaNode(String(), validator=Length(min=3, max=300))
+municipality_code = SchemaNode(Int())
+province = SchemaNode(String(), validator=Length(min=5, max=100))
+website = SchemaNode(String(), validator=url)
 
 class Address(MappingSchema):
     street = SchemaNode(String(), validator=Length(min=4, max=300))
     city = SchemaNode(String(), validator=Length(min=3, max=300))
     zip_code = SchemaNode(String(), validator=Length(min=6, max=6))
+
+
+class EducationStructures(SequenceSchema):
+    structure = SchemaNode(String())
