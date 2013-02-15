6347c8b3d263b5ea91934356346c351f797e86e0
Bart de Goede <bdegoede@gmail.com>
2013-02-15 12:59:46 +0100

    Pipeline supports multiple export methods now, and tarring and removal of files

--- onderwijsscrapers/onderwijsscrapers/pipelines.py
+++ onderwijsscrapers/onderwijsscrapers/pipelines.py
@@ -1,12 +1,15 @@
+import glob
 import os
 from datetime import datetime
 import pytz
+import shutil
+import tarfile
 
 from scrapy.conf import settings
 from scrapy.exceptions import DropItem
 from scrapy import log
 
-from onderwijsscrapers import exporters
+# from onderwijsscrapers import exporters
 from onderwijsscrapers.item_enrichment import bag42_geocode
 
 
@@ -20,7 +23,7 @@ class OnderwijsscrapersPipeline(object):
     def process_item(self, item, spider):
         # Check if the fields that identify the item are present. If not,
         # log and drop the item.
-        id_fields = settings['ELASTIC_SEARCH'][spider.name]['id_fields']
+        id_fields = settings['EXPORT_SETTINGS'][spider.name]['id_fields']
         if not all(field in item for field in id_fields):
             log.msg('Dropped item, not all required fields are present. %s'
                 % item, level=log.WARNING, spider=spider)
@@ -51,53 +54,62 @@ class OnderwijsscrapersPipeline(object):
         return item
 
     def close_spider(self, spider):
-        # Setup the exporter
-        if settings['EXPORT_METHOD'] == 'elasticsearch':
-            exporter = exporters.ElasticSearchExporter(
-                scrape_started_at=self.scrape_started,
-                spider=spider.name,
-                config=settings['ELASTIC_SEARCH'][spider.name],
-                url=settings['ELASTIC_SEARCH'][spider.name]['url'],
-                index=settings['ELASTIC_SEARCH'][spider.name]['index'],
-                doctype=settings['ELASTIC_SEARCH'][spider.name]['doctype'],
-            )
-
-        elif settings['EXPORT_METHOD'] == 'file':
-            exporter = exporters.FileExporter(
-                scrape_started_at=self.scrape_started,
-                spider=spider.name,
-                config=settings['ELASTIC_SEARCH'][spider.name],
-                url=settings['ELASTIC_SEARCH'][spider.name]['url'],
-                index=settings['ELASTIC_SEARCH'][spider.name]['index'],
-                doctype=settings['ELASTIC_SEARCH'][spider.name]['doctype'],
-                path=os.path.join(settings['EXPORT_DIR'], spider.name),
-            )
-
-        id_fields = settings['ELASTIC_SEARCH'][spider.name]['id_fields']
-        for item_id, item in self.items.iteritems():
-            universal_item = 'None-%s' % '-'.join([str(item[field]) for field in \
-                id_fields[1:]])
-            if universal_item in self.universal_items:
-                universal_item = self.universal_items[universal_item]
-                if 'reference_year' in universal_item:
-                    del universal_item['reference_year']
-                item.update(universal_item)
-
-            if 'ignore_id_fields' in item:
-                del item['ignore_id_fields']
-
-            item['meta'] = {
+        # Setup the exporters
+        for export_method, exporter in settings.get('EXPORT_METHODS').items():
+            export_settings = {
                 'scrape_started_at': self.scrape_started,
-                'item_scraped_at': datetime.utcnow().replace(tzinfo=pytz.utc)\
-                    .strftime('%Y-%m-%dT%H:%M:%SZ')
+                'spider': spider.name,
+                'config': settings['EXPORT_SETTINGS'][spider.name],
+                'url': settings['EXPORT_SETTINGS'][spider.name]['url'],
+                'index': settings['EXPORT_SETTINGS'][spider.name]['index'],
+                'doctype': settings['EXPORT_SETTINGS'][spider.name]['doctype']
             }
+            if export_method == 'file':
+                export_settings['path'] = os.path.join(settings['EXPORT_DIR'],
+                    spider.name)
 
-            # Geocode if enabled for this index
-            if settings['ELASTIC_SEARCH'][spider.name]['geocode']:
-                for address_field in settings['ELASTIC_SEARCH'][spider.name]['geocode_fields']:
-                    if address_field in item:
-                        geocoded = bag42_geocode(item[address_field])
-                        if geocoded:
-                            item[address_field].update(geocoded)
+            exporter = exporter['exporter'](**export_settings)
 
-            exporter.save(item_id, item)
+            id_fields = settings['EXPORT_SETTINGS'][spider.name]['id_fields']
+            for item_id, item in self.items.iteritems():
+                universal_item = 'None-%s' % '-'.join([str(item[field]) for field in \
+                    id_fields[1:]])
+                if universal_item in self.universal_items:
+                    universal_item = self.universal_items[universal_item]
+                    if 'reference_year' in universal_item:
+                        del universal_item['reference_year']
+                    item.update(universal_item)
+
+                if 'ignore_id_fields' in item:
+                    del item['ignore_id_fields']
+
+                item['meta'] = {
+                    'scrape_started_at': self.scrape_started,
+                    'item_scraped_at': datetime.utcnow().replace(tzinfo=pytz.utc)\
+                        .strftime('%Y-%m-%dT%H:%M:%SZ')
+                }
+
+                # Geocode if enabled for this index
+                if settings['EXPORT_SETTINGS'][spider.name]['geocode']:
+                    for address_field in settings['EXPORT_SETTINGS'][spider.name]['geocode_fields']:
+                        if address_field in item:
+                            geocoded = bag42_geocode(item[address_field])
+                            if geocoded:
+                                item[address_field].update(geocoded)
+
+                exporter.save(item_id, item)
+
+        # Tar files
+        if exporter['options']['tar']:
+            log.msg('Tarring JSON files', level=log.INFO, spider=spider)
+            # Tar the JSON files
+            scrape_started = datetime.strptime(self.scrape_started,
+                '%Y-%m-%dT%H:%M:%SZ').strftime('%Y%m%d%H%M%S')
+            with tarfile.open('%s/%s-%s.tar.gz' % (settings['EXPORT_DIR'],\
+                scrape_started, spider.name), 'w:gz') as tar:
+                for f in glob.glob('%s/*.json' % (spider.name)):
+                    tar.add(f)
+
+        # Remove files
+        if exporter['exporter']['options']['remove_json']:
+            shutil.rmtree('%s/%s' % (settings['EXPORT_DIR'], spider.name))
