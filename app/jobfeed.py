7e781dbef15129c9c30fa04c48f7f2fab47bf1e4
Benno Kruit <bennokr@gmail.com>
2014-12-12 14:47:19 +0100

    Added types, facet queries

--- app/jobfeed.py
+++ app/jobfeed.py
@@ -6,10 +6,20 @@ from settings import (ES_URL)
 
 es = rawes.Elastic(ES_URL)
 
+ES_INDEXES = ['jobfeed']
+ES_DOCUMENT_TYPES_PER_INDEX = {
+    'jobfeed': ['job'],
+}
+ES_DOCUMENT_TYPES = [d for i in ES_DOCUMENT_TYPES_PER_INDEX.values() for d in i]
 
 class JobSearch(restful.Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('q', type=str)
+    parser.add_argument('size', type=int, default=10)
+    parser.add_argument('from', type=int, default=0)
+    parser.add_argument('sort', type=str)
+    parser.add_argument('order', type=str, default='asc')
+
 
     text_fields = [
         "title",
@@ -29,12 +39,36 @@ class JobSearch(restful.Resource):
         "employment_type",
         "sector",
     ]
+    num_fields = [
+        "job_location_id",
+        "jobfeed_profession_id",
+        "jobfeed_profession_group_id",
+        "jobfeed_profession_class_id",
+        "source_type_id",
+        "education_level_id",
+        "employment_type_id",
+        "contract_type_id",
+        "working_hours_id",
+        "hours_per_week_min",
+        "hours_per_week_max",
+        "salary_min",
+        "salary_max",
+        "experience_min",
+        "experience_max",
+        "sector_id",
+        "org_size_id",
+        "sic",
+    ]
+    for t in text_fields:
+        parser.add_argument(t, type=str)
+    for n in num_fields:
+        parser.add_argument(n, type=int)
 
     def get(self):
         args = self.parser.parse_args()
 
-        args['indexes'] = 'jobfeed'
-        args['doctypes'] = 'job'
+        args['indexes'] = ','.join(ES_INDEXES)
+        args['doctypes'] = ','.join(ES_DOCUMENT_TYPES)
 
         query = {'query': {'filtered': {'query': {}}}}
         if args['q']:
@@ -46,11 +80,82 @@ class JobSearch(restful.Resource):
         else:
             query['query']['filtered']['query'] = {'match_all': {}}
 
+        for arg in (self.text_fields + self.num_fields):
+            if args[arg] is None:
+                continue
+
+            if 'filter' not in query['query']['filtered']:
+                query['query']['filtered']['filter'] = {'and': []}
+
+            arg_value = args[arg]
+            if type(arg_value) is str:
+                arg_value = arg_value.lower().split(' ')
+            if type(arg_value) is int:
+                arg_value = [arg_value]
+
+            query['query']['filtered']['filter']['and'].append({
+                'terms': {
+                    arg: arg_value,
+                    'execution': 'and'
+                }
+            })
+
+        # Number of hits to return and the offset
+        query['size'] = args['size']
+        query['from'] = args['from']
+
+        # Sort results
+        if args['order'] not in ['asc', 'desc']:
+            abort(400, message='Order can only be "asc" or "desc"')
+
+        if args['sort']:
+            if args['sort'] == 'distance':
+                if not args['geo_location']:
+                    abort(400, message='Cannot sort by distance if '
+                                       'geo_location is not provided.')
+
+                query['sort'] = [{
+                    '_geo_distance': {
+                        'address.geo_location': args['geo_location'],
+                        'order': args['order'],
+                        'unit': 'km'
+                    }
+                }]
+            else:
+                query['sort'] = [
+                    {args['sort']: {'order': args['order']}}
+                ]
+
         return es.get('%s/%s/_search'
            % (args['indexes'],
               args['doctypes']),
            data=query)
 
+class JobDoc(restful.Resource):
+    def get(self, index, doc_type, doc_id):
+        # Return an error if the requested index does not exist
+        if index not in ES_INDEXES:
+            abort(400, message='Index "%s" does not exist' % index)
+
+        # Return an error if the requested doctype does not exist in the
+        # requested index
+        if doc_type not in ES_DOCUMENT_TYPES_PER_INDEX[index]:
+            abort(400, message='Doctype "%s" does not exist in index "%s"'
+                               % (doc_type, index))
+ 
+        try:
+            doc = es.get('%s/%s/%s' % (index, doc_type, doc_id))
+        except rawes.elastic_exception.ElasticException, error:
+            if error.status_code == 404:
+                abort(404, message='The requested document does not exist')
+            else:
+                print(error)
+
+
+
+        return doc
+
 class JobFeed():
     def __init__(self, api):
         api.add_resource(JobSearch, '/api/v2/job_search')
+        api.add_resource(JobDoc, '/api/v2/job_doc/<index>/<doc_type>/<doc_id>')
--- vacaturescrapers/jobfeed.py
+++ vacaturescrapers/jobfeed.py
@@ -5,12 +5,13 @@ from datetime import datetime
 
 """
 Super quick 'n dirty shallow xml thing loader
+
+TODO:
+	lat/lon geo in ES
 """
 
 es = elasticsearch.Elasticsearch([{'host': 'localhost'}])
-def add(doc):
-	index = 'jobfeed_%s'%datetime.now().strftime('%y%m%d')
-	es.index(index=index, doc_type='job', body=doc)
+index = 'jobfeed_%s'%datetime.now().strftime('%y%m%d')
 
 print sys.argv[1]
 tree = ET.parse(sys.argv[1])
@@ -18,4 +19,19 @@ root = tree.getroot()
 
 for job in root:
 	print 'Adding job (id=%s)' % job.find('id').text
-	# add({f.tag: f.text for f in job})
+	body = {}
+	for f in job:
+		text = f.text
+		if text is not None:
+			if any(s in f.tag for s in ['_id','_min','_max']) or f.tag=='sic':
+				text = int(text)
+			if f.tag in ['expired_at', 'date']:
+				text = datetime.strptime( text, "%Y-%m-%d" )
+			body[f.tag] = text
+	# print body
+	es.index(index=index, doc_type='job', body=body)
+
+esi = elasticsearch.client.IndicesClient(es)
+if esi.exists_alias(name='jobfeed'):
+	esi.delete_alias(index='_all', name='jobfeed')
+esi.put_alias(index=index, name='jobfeed')
