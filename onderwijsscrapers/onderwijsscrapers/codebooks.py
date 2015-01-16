1fe7f92a712dade886d4dbdac3ffae6448d95b1b
Benno Kruit <bennokr@gmail.com>
2015-01-16 15:26:13 +0100

    make codebooks hierarchical with GroupNested datastructure

--- onderwijsscrapers/onderwijsscrapers/codebooks.py
+++ onderwijsscrapers/onderwijsscrapers/codebooks.py
@@ -1,7 +1,9 @@
-from collections import namedtuple, defaultdict
+from collections import namedtuple, defaultdict, OrderedDict
 import csv, StringIO
 import colander
+from colander import SchemaNode
 import re
+from onderwijsscrapers.groupnested import GroupNested
 
 default_datatypes = {
     'int': (int, colander.Int()), 
@@ -26,15 +28,66 @@ class Codebook(dict):
         for rule in field_dicts:
             rule['cast'], rule['typ'] = datatypes[rule.pop('type', 'string')]
             name = rule.pop('field', None)
+            if '.' in name:
+                name = tuple(name.split('.'))
             if name:
                 self[name] = Field(**rule)
 
+        # nested representation
+        def nest(key, val, thedict, nest_stack):
+            if nest_stack:
+                n = nest_stack.pop()
+                thedict.setdefault(n, {})
+                nest(key, val, thedict[n], nest_stack)
+            else:
+                thedict[key] = val
+        keyed_fields = set(name for name, f in self.items() if f.keyed)
+        keys = set(n for n,r in self.items() if int(r.keyed or 0) and r.source)
+        self.nested = {}
+        for n,r in self.iteritems():
+            if not r.keyed:
+                # add to tree by root keys and nested keys
+                nests = set(k for k in keyed_fields if '<%s>'%k in r.source)
+                stack = sorted(keys|nests, key=lambda k: -int(self[k].keyed))
+                nest(n,r, self.nested, stack)
+
+        self.root = namedtuple('Root', [n for n in self if self[n].keyed=='0'])
+
     def schema(self, root=False, **kwargs):
-        schema = colander.SchemaNode(typ=colander.Mapping(), **kwargs)
+        def to_validator(val, name=''):
+            if type(val) == Field:
+                return SchemaNode(typ=val.typ, name=name, missing=None)
+            elif type(val) == dict:
+                seq = SchemaNode(typ=colander.Sequence(), name = 'per_%s'%name)
+                out = SchemaNode(typ=colander.Mapping() , name = name)
+                tree = {}
+                def add_tree(k):
+                    """add mappings to `tree` with tuple key, return deepest"""
+                    if k not in tree:
+                        tree[k] = SchemaNode(typ=colander.Mapping(), name=k[-1])
+                        if k[:-1]: # add nested
+                            if k[:-1] not in tree:
+                                add_tree(k[:-1])
+                            tree[k[:-1]].add(tree[k]) # add schema to parent                        
+                        else:
+                            out.add(tree[k]) # add to root
+                    return tree[k]
+                for k,v in val.iteritems():
+                    if type(k) == tuple and k:
+                        node, name = add_tree(k[:-1]), k[-1]
+                    else:
+                        node, name = out, k
+                    node.add(to_validator(v, name))
+                seq.add(out)
+                return seq
+
+        schema = SchemaNode(typ=colander.Mapping(), **kwargs)
         schema.__doc__ = schema.description
+        for k,v in self.nested.items():
+            schema.add(to_validator(v,k))
         for name, field in self.items():
-            if root or field.keyed != '0':
-                schema.add(colander.SchemaNode(typ=field.typ, name=name, missing=None ))
+            if root and field.keyed == '0':
+                schema.add(SchemaNode(typ=field.typ, name=name))
         return schema
 
     def parse(self, heads, rows):
@@ -42,63 +95,98 @@ class Codebook(dict):
         keyed_per_head = defaultdict(list)
         unkeyed_per_head = defaultdict(list)
         for i,head in enumerate(heads):
-            for fieldname, field in self.iteritems():
+            for name, field in self.iteritems():
                 if field.source:
                     m = re.match(field.source, head)
                     if m: # add non-trivial matches
                         if field.keyed:
-                            keyed_per_head[i].append(fieldname)
+                            keyed_per_head[i].append(name)
                         else:
-                            unkeyed_per_head[i].append((fieldname, m.groupdict()))
+                            unkeyed_per_head[i].append((name, m.groupdict()))
+
+        # Make nested dataset for grouping values
+        def no_fields(nest):
+            return {k:no_fields(v) if type(v)==dict else False
+                for k,v in nest.iteritems()}
+        def sortkey(k):
+            return int(self[k[0]].keyed or 0)
+        # store items with root-keys as key
+        dataset = GroupNested({'root': no_fields(self.nested)})
 
         for row in rows:
-            yield self.parse_row(row, keyed_per_head, unkeyed_per_head)
+            parse = self.parse_row(row, keyed_per_head, unkeyed_per_head)
+            for key, nest, up in parse:
+                key = [('root', self.root(**key))]
+                key += sorted(nest.items(), key=sortkey)
+                dataset[OrderedDict(key)].update(up)
 
+        for key, data in dataset.store['root'].iteritems():
+            yield dict(key._asdict()), data.struct()
+
+        
 
     def parse_row(self, row, keyed_per_head, unkeyed_per_head):
         key = {}
+        root = {}
         # fill key with keyed fields
         for i, fieldnames in keyed_per_head.iteritems():
             for fieldname in fieldnames:
                 for val in self[fieldname].get_value(row[i]):
-                    key[fieldname] = val
+                    if self[fieldname].keyed == '0':
+                        root[fieldname] = val
+                    else:
+                        key[fieldname] = val
         # parse rest of fields
         for i, fieldnames in unkeyed_per_head.iteritems():
             for (fieldname, groups) in fieldnames:
                 for val in self[fieldname].get_value(row[i]):
-                    nest_key = { name: nest_val
+                    key.update({ name: nest_val
                         for name, nest_str in groups.iteritems()
                         for nest_val in self[name].get_value(nest_str)
-                    }
-                    if nest_key:
-                        nest_key.update(key)
-                        key = nest_key
-                    return key, {fieldname: val}
+                    })
+                    yield root, key, {fieldname: val}
 
 if __name__ == '__main__':
     import sys
-    book = """field;keyed;source;type
-board_id;0;BEVOEGD GEZAG NUMMER;int
-vavo;;AANTAL LEERLINGEN;int
-"""
 
-    table = """BEVOEGD GEZAG NUMMER;AANTAL LEERLINGEN
-1;10
-2;20
-"""
-
-    cb = CodebookMini(csv.DictReader(StringIO.StringIO(book), delimiter=';'))
-    print cb
-
-    print ''
     def print_schema(sch, n=0):
         print ' '*n, sch.name, '(%s)' % type(sch.typ).__name__
         for s in sch:
             if type(sch.typ).__name__ in ['Mapping', 'Sequence']:
                 print_schema(s, n+1)
-    
-    print_schema(cb.schema(root=True))
 
+    book = """field;keyed;source;type
+board_id;0;BEVOEGD GEZAG NUMMER;int
+vavo;;AANTAL LEERLINGEN;int
+"""
+    table = """BEVOEGD GEZAG NUMMER;AANTAL LEERLINGEN
+1;10
+2;20
+"""
+
+    cb = Codebook(csv.DictReader(StringIO.StringIO(book), delimiter=';'))
+    print cb
+    print_schema(cb.schema(root=True))
+    t = csv.reader(StringIO.StringIO(table), delimiter=';')
+    for l in cb.parse(next(t), t):
+        print l
+
+    # artificial
+    book = """field;keyed;source;type
+board_id;0;BEVOEGD GEZAG NUMMER;int
+reference_year;0;JAAR;int
+student_year;1;LEERJAAR;int
+vavo;;AANTAL LEERLINGEN;int
+non_vavo;;NIET VAVO;int
+"""
+    table = """BEVOEGD GEZAG NUMMER;JAAR;LEERJAAR;AANTAL LEERLINGEN;NIET VAVO
+1;2014;1;10;40
+2;2014;1;20;70
+2;2014;2;4;34
+"""
+    cb = Codebook(csv.DictReader(StringIO.StringIO(book), delimiter=';'))
+    print cb
+    print_schema(cb.schema(root=True))
     t = csv.reader(StringIO.StringIO(table), delimiter=';')
     for l in cb.parse(next(t), t):
         print l
